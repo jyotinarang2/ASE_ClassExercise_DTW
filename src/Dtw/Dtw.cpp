87655b5a733b9609c57ff432ae84afbb65ab393e
unknown <jyotinarang.nsit@gmail.com>
2018-04-04 23:24:40 -0400

    Add DTW assignment

--- src/Dtw/Dtw.cpp
+++ src/Dtw/Dtw.cpp
@@ -3,44 +3,132 @@
 #include "Util.h"
 
 #include "Dtw.h"
-
-CDtw::CDtw( void )
+#include<iostream>
+using namespace std;
+CDtw::CDtw(void)
 {
 
 }
 
-CDtw::~CDtw( void )
+CDtw::~CDtw(void)
 {
 
 }
 
-Error_t CDtw::init( int iNumRows, int iNumCols )
+Error_t CDtw::init(int iNumRows, int iNumCols)
 {
-    return kNoError;
+	//bIsInit = 1;
+	ppfCost = new float*[iNumRows];
+	for (int i = 0; i < iNumRows; i++)
+		ppfCost[i] = new float[iNumCols];
+
+	iTracebackPath = new int*[iNumRows];
+	for (int i = 0; i < iNumRows; i++)
+		iTracebackPath[i] = new int[iNumCols];
+
+
+	for (int i = 0; i < iNumRows; i++) {
+		for (int j = 0; j < iNumCols; j++) {
+			ppfCost[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < iNumRows; i++) {
+		for (int j = 0; j < iNumCols; j++) {
+			iTracebackPath[i][j] = 0;
+		}
+	}
+	iMatrixDimensions[MatrixDimension_t::kRow] = iNumRows;
+	iMatrixDimensions[MatrixDimension_t::kCol] = iNumCols;
+	return kNoError;
 }
 
 Error_t CDtw::reset()
 {
-    return kNoError;
+	return kNoError;
 }
 
 Error_t CDtw::process(float **ppfDistanceMatrix)
 {
-    return kNoError;
-}
+	/*if (!bIsInit)
+	{
+		return kNotInitializedError;
+	}*/
+	ppfCost[0][0] = ppfDistanceMatrix[0][0];
 
+	for (int i = 1; i < iMatrixDimensions[kCol]; i++)
+	{
+		ppfCost[0][i] = ppfCost[0][i - 1] + ppfDistanceMatrix[0][i];
+		iTracebackPath[0][i] = 1;
+	}
+
+	for (int j = 1; j < iMatrixDimensions[kRow]; j++)
+	{
+		ppfCost[j][0] = ppfCost[j - 1][0] + ppfDistanceMatrix[j][0];
+		iTracebackPath[j][0] = 2;
+	}
+
+	for (int i = 1; i < iMatrixDimensions[kRow]; i++)
+	{
+		for (int j = 1; j < iMatrixDimensions[kCol]; j++)
+		{
+			float min_value = computeMinimum(ppfCost[i - 1][j - 1], ppfCost[i][j - 1], ppfCost[i - 1][j]);
+			iTracebackPath[i][j] = iCurrentDirection;
+
+			ppfCost[i][j] = ppfDistanceMatrix[i][j] + min_value;
+		}
+	}
+	
+	return kNoError;
+}
+float CDtw::computeMinimum(float a, float b, float c)
+{
+
+	float min = a;
+	iCurrentDirection = 0;
+
+	if (a>b)
+	{
+		min = b;
+		iCurrentDirection = 1;
+	}
+
+	if (c < min)
+	{
+		min = c;
+		iCurrentDirection = 2;
+	}
+	return min;
+}
 int CDtw::getPathLength()
-{    
-    return 0;
+{
+
+	int count = 0;
+	int columns = iMatrixDimensions[kCol]-1;
+	int rows = iMatrixDimensions[kRow]-1;
+	while (columns > 0 || rows > 0) {
+		if (iTracebackPath[rows][columns] == 0) {
+			--columns; --rows;
+			count++;
+		}
+		if (iTracebackPath[rows][columns] == 1) {
+			--columns;
+
+		}
+		if (iTracebackPath[rows][columns] == 2) {
+			--rows;
+			count++;
+		}
+	}
+	return count+1;
 }
 
 float CDtw::getPathCost() const
 {
-    return 0.f;
+	return ppfCost[iMatrixDimensions[0]-1][iMatrixDimensions[1]-1];
 }
 
-Error_t CDtw::getPath( int **ppiPathResult ) const
+Error_t CDtw::getPath(int **ppiPathResult) const
 {
-    return kNoError;
+	return kNoError;
 }
 
--- src/inc/Dtw.h
+++ src/inc/Dtw.h
@@ -11,60 +11,68 @@
 class CDtw
 {
 public:
-    enum MatrixDimension_t
-    {
-        kRow,
-        kCol,
+	enum MatrixDimension_t
+	{
+		kRow,
+		kCol,
 
-        kNumMatrixDimensions
-    };
+		kNumMatrixDimensions
+	};
 
-    CDtw(void);
-    virtual ~CDtw(void);
+	CDtw(void);
+	virtual ~CDtw(void);
 
-    /*! initializes the class with the size of the distance matrix
-    \param iNumRows
-    \param iNumCols
-    \return Error_t
-    */
-    Error_t init (int iNumRows, int iNumCols);
-    
-    /*! resets all internal class members
-    \return Error_t
-    */
-    Error_t reset ();
+	/*! initializes the class with the size of the distance matrix
+	\param iNumRows
+	\param iNumCols
+	\return Error_t
+	*/
+	Error_t init(int iNumRows, int iNumCols);
 
-    /*! computes cost and path w/o back-tracking
-    \param ppfDistanceMatrix (dimensions [rows][columns])
-    \return Error_t
-    */
-    Error_t process (float **ppfDistanceMatrix);
- 
-    /*! returns the length of the path
-    \return int
-    */
-    int getPathLength ();
-    
-    /*! returns the overall cost
-    \return float
-    */
-    float getPathCost () const;
-    
-    /*! returns the path \sa getPathLength
-    \param ppiPathResult pointer to memory the result is written to (dimensions [2][length_of_path])
-    \return Error_t
-    */
-    Error_t getPath (int **ppiPathResult) const;
+	/*! resets all internal class members
+	\return Error_t
+	*/
+	Error_t reset();
+
+	/*! computes cost and path w/o back-tracking
+	\param ppfDistanceMatrix (dimensions [rows][columns])
+	\return Error_t
+	*/
+	Error_t process(float **ppfDistanceMatrix);
+
+	/*! returns the length of the path
+	\return int
+	*/
+	int getPathLength();
+
+	/*! returns the overall cost
+	\return float
+	*/
+	float getPathCost() const;
+
+	/*! returns the path \sa getPathLength
+	\param ppiPathResult pointer to memory the result is written to (dimensions [2][length_of_path])
+	\return Error_t
+	*/
+	Error_t getPath(int **ppiPathResult) const;
+
+	float computeMinimum(float a, float b, float c);
 
 private:
-    enum Directions_t
-    {
-        kHoriz,
-        kVert,
-        kDiag,
 
-        kNumDirections
-    };
+	int iMatrixDimensions[kNumMatrixDimensions];
+	float **ppfCost;
+	int **iTracebackPath;
+	int iCurrentDirection;
+	bool bIsInit = false;
+	enum Directions_t
+	{
+		kHoriz,
+		kVert,
+		kDiag,
+
+		kNumDirections
+	};
 };
 
 
